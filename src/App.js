bee27cf90f7192f981761b26e4d48b397a860e0b
Pruthviraj Patil <pruthviraj.patil@goapptiv.in>
2023-12-24 00:10:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import './App.css'
 
-function App() {
+const ImageUpload = () => {
+  const [imagePreview, setImagePreview] = useState("logo192.png");
+
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+
+    if (file) {
+      const reader = new FileReader();
+
+      reader.onload = () => {
+        setImagePreview(reader.result);
+      };
+
+      reader.readAsDataURL(file);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="mainBox"> 
+        <h1>Fwish App</h1>
+        <Card className="fishCard" style={{ width: "60%" }}>
+          <div>
+            <img
+              src={imagePreview}
+              style={{
+                maxWidth: "300px",
+                maxHeight: "300px",
+                marginBottom: "20px",
+              }}
+              alt="ImagePreview"
+            />
+            <br />
+          </div>
+          <Card.Title>Insert your fish image to get amazing results</Card.Title>
+          {/* <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text> */}
+          <div className="btnbox">
+            <label
+              style={{
+                cursor: "pointer",
+                padding: "10px 15px",
+                backgroundColor: "#3498db",
+                color: "white",
+                borderRadius: "5px",
+                fontSize: "16px",
+              }}
+            >
+              Upload 🐡
+              <Button
+               
+                as="input"
+                type="file"
+                style={{ display: "none" }}
+                accept="image/*"
+                onChange={handleImageChange}
+              />
+            </label>
+            <Button className="succesBtn" variant="success">Get Result 😁</Button>{" "}
+          </div>
+        </Card>
+      </div>
+    </>
   );
-}
+};
 
-export default App;
+export default ImageUpload;
